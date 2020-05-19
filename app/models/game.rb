7a4378ffd40a59b32ea44f3418997cb2d552c2c8
Freddy Munive <munivef@yahoo.com>
2020-05-19 18:15:28 -0500

    adding validators to game

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,5 @@
 class Game < ApplicationRecord
-    
+    validates :name, :gender, :price, :release_date, presence: true
+    validates :gender, /("Simulator"|"Adventure"|"Strategy"|"Role-playing (RPG)"|"Shooter"|"Fighting"|"Sport")/
+    validates :price, only_integer: true
 end
