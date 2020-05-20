8cc003701d4b537c37e966d929f913daef5dcce1
Freddy Munive <munivef@yahoo.com>
2020-05-20 11:43:54 -0500

    adding controllers to games and companies

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,5 +1,7 @@
 class Game < ApplicationRecord
     validates :name, :gender, :price, :release_date, presence: true
-    validates :gender, format: { width: /(Simulator|Adventure|Strategy|Role\-playing\s{1}\(RPG\)|Shooter|Fighting|Sport)/}
-    validates :price, only_integer: true
+    # validates :gender, format: { width: /(Simulator|Adventure|Strategy|Role\-playing\s{1}\(RPG\)|Shooter|Fighting|Sport)/}
+    validates :gender, inclusion: {in: ["Simulator", "Adventure", "Strategy", "Role playing (RPG)", "Shooter", "Fighting", "Sport"] }
+    validates :price, numericality: {only_integer: true }
+    belongs_to :company, optional: true
 end
