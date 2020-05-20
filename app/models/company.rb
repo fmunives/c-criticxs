754e8ddda3a9eb4deb72514bdc2c88eaa213672b
Freddy Munive <munivef@yahoo.com>
2020-05-19 22:04:54 -0500

    correct the max length of companies

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,4 +1,4 @@
 class Company < ApplicationRecord
     validates :name, :country, presence: true
-    validates :name, uniqueness: true, length: {minimum: 40}
+    validates :name, uniqueness: true, length: {maximum: 40}
 end
